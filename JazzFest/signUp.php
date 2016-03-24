1f62b147b15c6a0e31f1c443f549806fec194ae7
Zach <zbgreen7@gmail.com>
2016-03-23 22:58:27 -0500

    Finished signUp.html and .php

--- JazzFest/signUp.php
+++ JazzFest/signUp.php
@@ -5,76 +5,119 @@
  * Date: 3/22/16
  * Time: 11:31 PM
  */
+process();
 
-//Form variables
-$form = array("School name" => $_POST["school_name"],
-    "School enrollment" => $_POST["school_enrollment"],
-    "Distance from Kirksville" => $_POST["distance"],
-    "Name of director(s)" => $_POST["director_name"],
-    "Contact address" => $_POST["address"],
-    "Phone" => $_POST["phone"],
-    "Fax" => $_POST["fax"],
-    "E-mail" => $_POST["email"],
-    "E-mail me" => $_POST["email_me"],
-    "Ensemble 1" => $_POST["ensemble_1"],
-    "Ensemble 2" => $_POST["ensemble_2"],
-    "Ensemble 3" => $_POST["ensemble_3"],
-    "Ensemble 4" => $_POST["ensemble_4"],
-    "Ensemble 5" => $_POST["ensemble_5"],
-    "Combo 1" => $_POST["combo_1"],
-    "Combo 2" => $_POST["combo_2"],
-    "Combo 3" => $_POST["combo_3"],
-    "Combo 4" => $_POST["combo_4"],
-    "Combo 5" => $_POST["combo_5"]);
+function process() {
+    echo "<h1>Jazz Fest Online Application</h1>";
 
-process($form);
-
-function process($form) {
-    $field = checkBlanks($form);
-    if ($field !== false) {
-        printError($field);
-    } else {
-        printApp($form);
+    //assign checkbox variable.
+    $email_me = false;
+    if ( (isset($_POST['email_me'])) && ($_POST['email_me'] === "email_me") ) {
+        $email_me = true;
     }
-}
+    //Form variables
+    $form = ["school_name",
+        "school_enrollment",
+        "distance",
+        "director",
+        "address",
+        "phone",
+        "email",
+        "ensemble_1",
+        "ensemble_2",
+        "ensemble_3",
+        "ensemble_4",
+        "ensemble_5",
+        "combo_1",
+        "combo_2",
+        "combo_3",
+        "combo_4",
+        "combo_5"];
 
-//Check if any of the fields were left blank.
-function checkBlanks($form) {
-    $field = null;
+    $complete = reqFields($form);
+    $bands = hasBands($form);
 
-    for ($i = 0; $i <= 7; $i++) {
-        if ($form[$i] === "") {
-            $field = $i;
+    //true if complete application.
+    if ($complete && $bands) {
+        //optional email confirmation
+        printApp($form);
+        if ($email_me) {
+            sendEmail($form);
         }
     }
-    //If a field is blank return the field name
-    if ($field !== null) {
-        return key($form);
-    } else {
-        return false;
+}
+
+//Check if the field is empty.
+function reqFields($form)
+{
+    $status = true;
+    //loop through first 7 required fields
+    for ($i = 0; $i <= 6; $i++) {
+        //true if not set or empty
+        if (!isset($_POST[$form[$i]]) || empty($_POST[$form[$i]])) {
+            //if first missing field print out error header.
+            if ($status) {
+                echo <<<END
+                    <h3>Missing field Notification</h3>
+                    <p>You did not complete the following required field(s):</p>
+END;
+                $status = false;
+            }
+            echo "<p>$form[$i]</p>";
+        }
+    }
+    return $status;
+}
+
+//Make sure schools have at least 1 band.
+function hasBands($form) {
+    $status = false;
+
+    for ($i = 7; $i < count($form); $i++) {
+        if (!empty($_POST[$form[$i]])) {
+            $status = true;
+        }
+    }
+    if (!$status) {
+        echo <<<END
+            <h3>Missing Band Notification</h3>
+            <p>You have not signed up any bands.</p>
+END;
+    }
+    return $status;
+}
+
+//Prints entered information to the user. Uses htmlspecialchars to prevent injections.
+function printApp($form) {
+    echo "<h3>Application Confirmation</h3>";
+    for ($i = 0; $i < count($form); $i++) {
+        $fieldName = $form[$i];
+        $field = htmlspecialchars($_POST[$form[$i]]);
+        if (!empty($field)) {
+            echo "<p>$fieldName: $field</p>";
+        }
     }
 }
 
-//Prints out the field name that wasn't filled out.
-function printError($field) {
-    echo <<<END
-    <h1>Jazz Fest Online Application</h1>
-    <h3>Application Error Notification</h3>
-    <p>You did not complete the $field field.</p>
-END;
-}
-
-function printApp($form) {
-    echo <<<END
-    <h1>Jazz Fest Online Application</h1>
-    <h3>School and Director Information</h3>
-    <table>
-END;
-
-    for ($i = 0; $i <= 8; $i++) {
-        $key = key($form);
-        $field = $form[$i];
-        echo "<tr><td>$key</td><td>$field</td></tr>";
+//Sends email to entered address. Uses htmlspecialchars to prevent injections.
+function sendEmail($form) {
+    $address = $_POST[$form[7]];
+    $subject = "JazzFest Application Confirmation";
+    echo "<p>Email Status: </p>";
+    //construct message
+    $message = "";
+    for ($i = 0; $i < count($form); $i++) {
+        $field = htmlspecialchars($_POST[$form[$i]]);
+        if (!empty($field)) {
+            $message .= $form[$i] . ": " . $field . "\r\n";
+        }
+    }
+    $success = mail($address, $subject, $message);
+    if ($success) {
+        echo "<h3>Email sent.</h3>";
+    }
+    else {
+        echo "<h3>Email failed to send. Contact administrator.</h3>";
     }
 }
 ?>
